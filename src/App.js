c6619de7c26ff9d2cf836d8edf7e6787dbe45ae9
VaibhavGupta911 <guptaayush519@gmail.com>
2023-07-08 22:43:03 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-
+const Button = ({ type, children, ...Buttonprops }) => {
+  const classname = type === "primary" ? "Primary Button" : "Secondary";
+  return (
+    <button className={classname} {...Buttonprops} >
+      {children}
+    </button>
+  )
+}
+const Login = ({ type, children, ...Buttonprops }) => {
+  return (
+    <Button {...Buttonprops} onClick={() => { alert("loged in ") }}>
+      {children}
+    </Button>
+  );
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2 className="App-header">
+        Spread Attribute use
+      </h2>
+      <Button type="primary" onClick={() => { alert("Signed in ") }}><h2>SignUP</h2></Button>
+      <Login ><h2>Login</h2></Login>
     </div>
   );
 }
